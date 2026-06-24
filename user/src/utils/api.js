3554afd805580af438aeba99e54e1d9a5b3a91a3
SaurabhArt111 <saurabhma168@gmail.com>
2026-06-24 15:37:25 +0530

    user api.js updated

--- user/src/utils/api.js
+++ user/src/utils/api.js
@@ -1,21 +1,32 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: '/api' })
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  'http://localhost:5900/api'
+
+const api = axios.create({
+  baseURL
+})
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('employeeToken')
-  if (token) cfg.headers.Authorization = `Bearer ${token}`
+
+  if (token) {
+    cfg.headers.Authorization = `Bearer ${token}`
+  }
+
   return cfg
 })
 
 api.interceptors.response.use(
-  r => r,
+  res => res,
   err => {
     if (err.response?.status === 401) {
       localStorage.removeItem('employeeToken')
       localStorage.removeItem('employeeUser')
       window.location.href = '/login'
     }
+
     return Promise.reject(err)
   }
 )
